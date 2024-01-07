92b1d1685b4a469f6be42b26c54380cf15ef93e2
Altie <altielynn@gmail.com>
2024-01-06 22:15:00 -0700

    first

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>s</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 52, 51, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
